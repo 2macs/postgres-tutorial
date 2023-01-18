10045d8f54ae8f6676a0f02d66aa137274fb788e
2macs <adrian.mcneill@outlook.com>
2023-01-18 15:54:07 +0000

    add sql-crud.py

---
+++ sql-crud.py
@@ -0,0 +1,61 @@
+from sqlalchemy import (
+    create_engine, Column, Float, ForeignKey, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# Create a new table called programmer, celebrate the great programmers
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
+
+
+# create new records in the programmer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer",
+)
+
+# Add each instance of programmer to the session
+session.add(ada_lovelace)
+
+
+# Commit each instance to the DBase
+session.commit()
+
+
+# Query the Database to find all programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+        )
